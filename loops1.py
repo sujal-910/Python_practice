80dd15cdea6553f339fbfea9ad0a2c8826db775e
Sujal Salvi <sujalsalvi910@.com>
2025-10-17 08:47:07 +0530

    This is my loops file

---
+++ loops1.py
@@ -0,0 +1,22 @@
+#  For syntax
+# for item in collection:
+#     print(item)
+
+
+# l=[23,35,2,4,6,7]
+
+# for i in l:
+#     print(i)
+
+
+
+# l=[3,35,2,4,6,7]
+
+# for i in l:
+#     print(i,end=" ")
+
+
+# l=[23,35,2,4,6,7]
+
+# for i in l:
+#     print(i,end="\t")
