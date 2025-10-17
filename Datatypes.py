00ae1f24accff0b035a8d52683c387334bf36c6e
Sujal Salvi <sujalsalvi910@.com>
2025-10-17 09:03:15 +0530

    This is my datatype file

---
+++ Datatypes.py
@@ -0,0 +1,117 @@
+
+# DATA TYPES - tyes of data
+
+# Integer
+# a=10
+# print(a)
+# print(type(a))
+
+# Float
+# b=23.1
+# print(b)
+# print(type(b))
+
+# String
+# c="Python"
+# print(c)
+# print(type(c))
+
+# b="23.1"
+# print(b)
+# print(type(b))
+
+# Boolean
+
+# a=True
+# print(type(a))
+
+# b=False
+# print(type(b))
+
+# Complex number(j represent complex number. only j is given)
+
+# a=2+3j
+# print(type(a))
+
+# a=2+3j
+# print(type(a))
+# print(a.imag)
+# print(a.real)
+
+# List is represented by []
+# we can add every type of data type in the list
+
+# l=[]
+# print(l) 
+# print(type(l))
+
+# l=[1,2,3,4,"string",23.4,True,3+5j]
+# print(l) 
+
+# print(type(l))
+#Tuple is  represented by ()
+
+# t=()
+# print()
+# print(type(t))
+
+# (when we add one element it integer. when we want to add one element as tuple the add ',' then it become tuple  )
+# t=(12)       
+# print(t)
+# print(type(t))
+
+
+# t=(1,2,3,4,"string",23.4,True,3+5j)
+# print(t) 
+# print(type(t))
+
+# Set is  represented by {}
+# empty {} is dictionary 
+# in set sort the sequence 
+# reparted value are only print once
+
+# s={12,4,4,4,"string",23.4,True,3+5j}
+# print(s)
+# print(type(s))
+
+# Dictionary are represted by {}
+# In dictinory values are in {key:value} form .
+
+# d={} 
+# print(d)
+# print(type(d))
+
+# d={"key":"value","name":"sujal","age":"22"} 
+# print(d)
+# print(type(d))
+
+# Length
+# In length space is also count .
+
+#List
+# l=[1,2,3,4,"Python"]
+# print(l)
+# print(len(l))
+
+# Tuple
+# l=(1,2,3,4,"Python")
+# print(l)
+# print(len(l))
+
+
+# d={"key":"value","name":"sujal","age":"22"} 
+# print(d)
+# print(len(d))
+ 
+# String
+# print(len("Python"))
+ 
+
+#int
+# a=10       
+# print(len(a))
+#   - error (int has no length)
+
+# int , boolen , float , complax we cannot check length . 
+
+
